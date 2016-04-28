10a226a2bec97b4cb7b79f8179a20e40e419fa39
Marius Olbertz <marius.olbertz@gmail.com>
2016-04-28 21:19:45 +0200

    Updated references to be relative.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
   browser = require('browser-sync'),
   requireDir = require('require-dir'),
   panini = require('panini'),
+  exec = require('child_process').exec,
   config = require('./gulp/config'),
   port = process.env.SERVER_PORT || 3000;
 
@@ -18,6 +19,13 @@ gulp.task('build', ['pages', 'scss', 'js']);
 
 gulp.task('serve', ['build'], function() {
   browser.init({server: config.destPath, port: port});
+});
+
+gulp.task('publish', function(cb) {
+  exec('git subtree push --prefix dist origin gh-pages', function(error, stdout, stderr) {
+    console.log(stdout);
+    cb();
+  });
 });
 
 gulp.task('watch', function() {
